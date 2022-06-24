55b5b39e64b40dcb080f88c1c5bb60eebeb7c424
Nanokungthai <105914130+Nanokungthai@users.noreply.github.com>
2022-06-24 16:03:47 +0700

    Create CS01-3.py

---
+++ CS01-3.py
@@ -0,0 +1,5 @@
+print("--------------------------------------------")
+A = int(input("you is your birthdate:"))
+print("your birthdate is:",A)
+print("( ͡❛ ‿‿ ͡❛)")
+print("--------------------------------------------")
