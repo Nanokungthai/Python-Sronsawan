e33271989616530ef8d8363619ac9aaaa7ae16d0
Nanokungthai <105914130+Nanokungthai@users.noreply.github.com>
2022-08-19 16:26:51 +0700

    Create Cs01-7.py

---
+++ Cs01-7.py
@@ -0,0 +1,9 @@
+print(".................................................................")
+a = int(input("Inout your frist number: "))
+b = int(input("Inout your second number: "))
+c = int(input("Inout your last number: "))
+if b < a < c:
+    print("condition is ture")
+else:
+    print("your condition is false")
+print(".................................................................")
